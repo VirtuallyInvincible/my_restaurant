4d5d93f44439667cc26ff5cf4fd4d3e455e372cb
Shai Mahfud <shai_mahfud@hotmail.com>
2021-06-05 21:20:05 +0300

    Added a new home component and website navigation in both the footer and the header. Now the user can navigate between different views.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,8 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishDetailsComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
@@ -19,10 +21,20 @@ class Main extends Component {
     }
 
     render() {
+        const HomePage = () => {
+          return (
+            <Home />
+          );
+        }
+
         return (
           <div>
             <Header />
-            <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+            <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Redirect to='/home' />
+            </Switch>
             <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
             <Footer />
           </div>
